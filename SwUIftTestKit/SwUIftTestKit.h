8f9205e9bf06c3ce8dcb29f41e9c4d7dc07fdd8a
hgsong <hgsong@wantedlab.com>
2018-07-26 12:32:56 +0900

    Initial Commit

---
+++ SwUIftTestKit/SwUIftTestKit.h
@@ -0,0 +1,19 @@
+//
+//  SwUIftTestKit.h
+//  SwUIftTestKit
+//
+//  Created by Ross on 2018. 7. 26..
+//  Copyright © 2018년 wanted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwUIftTestKit.
+FOUNDATION_EXPORT double SwUIftTestKitVersionNumber;
+
+//! Project version string for SwUIftTestKit.
+FOUNDATION_EXPORT const unsigned char SwUIftTestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwUIftTestKit/PublicHeader.h>
+
+
